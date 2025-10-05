b08bbb87a1e2bfe4ad020a7a50eaf2a0c24cec6a
Samer Jawadi <75566653+samerjawadi@users.noreply.github.com>
2025-10-05 09:05:03 +0100

    lates

--- AdvancedProgramming/Views/AdvancedProgrammingView.xaml.cs
+++ AdvancedProgramming/Views/AdvancedProgrammingView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using AdvancedProgramming.Events;
+﻿using AdvancedProgramming.Communs;
+using AdvancedProgramming.Events;
 using AdvancedProgramming.ViewModels;
 using ICSharpCode.AvalonEdit;
 using ICSharpCode.AvalonEdit.CodeCompletion;
@@ -9,35 +10,40 @@ using System;
 using System.Collections.Generic;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace AdvancedProgramming.Views
 {
+    /// <summary>
+    /// Interaction logic for AdvancedProgrammingView
+    /// </summary>
     public partial class AdvancedProgrammingView : UserControl
     {
         private readonly IEventAggregator _eventAggregator;
         private bool mustUpdate = true;
+
+        // Completion
         private CompletionWindow _completionWindow;
 
         public AdvancedProgrammingView(IEventAggregator eventAggregator)
         {
             InitializeComponent();
+
             _eventAggregator = eventAggregator;
 
             if (DataContext is AdvancedProgrammingViewModel vm)
                 vm.Subscribe(_eventAggregator);
 
             // Subscribe to events
-            eventAggregator.GetEvent<ScriptLoadedEvent>().Subscribe(script =>
+            eventAggregator.GetEvent<ScriptLoadedEvent>().Subscribe((script) =>
             {
                 mustUpdate = false;
                 TextEditor.Text = script;
                 mustUpdate = true;
             });
 
-            // Hook Ctrl+Space for autocomplete
+            // Hook Ctrl+Space
             TextEditor.TextArea.KeyDown += TextArea_KeyDown;
-            // Hook Enter to replace the current word with selected autocomplete
-            TextEditor.TextArea.PreviewKeyDown += TextArea_PreviewKeyDown;
         }
 
         private void TextEditor_TextChanged(object sender, System.EventArgs e)
@@ -51,7 +57,7 @@ namespace AdvancedProgramming.Views
                 _eventAggregator.GetEvent<ScriptChangedEvent>().Publish(TextEditor.Text);
         }
 
-        // Ctrl+Space triggers autocomplete
+        // === Ctrl+Space Trigger ===
         private void TextArea_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Space && (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control)
@@ -66,6 +72,7 @@ namespace AdvancedProgramming.Views
             _completionWindow = new CompletionWindow(TextEditor.TextArea);
             IList<ICompletionData> data = _completionWindow.CompletionList.CompletionData;
 
+            // Add Python keywords and builtins
             string[] pythonKeywords =
             {
                 "def", "class", "import", "from", "as",
@@ -82,37 +89,7 @@ namespace AdvancedProgramming.Views
             _completionWindow.Show();
             _completionWindow.Closed += delegate { _completionWindow = null; };
         }
-
-        // Replace current word with selected autocomplete on Enter
-        private void TextArea_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            if (_completionWindow != null && e.Key == Key.Enter)
-            {
-                // Remove the current word before inserting
-                var caretOffset = TextEditor.CaretOffset;
-                var start = TextEditor.Text.LastIndexOfAny(new char[] { ' ', '\n', '\r', '\t' }, caretOffset - 1) + 1;
-                if (start < 0) start = 0;
-
-                TextEditor.Select(start, caretOffset - start);
-                _completionWindow.CompletionList.RequestInsertion(e);
-                e.Handled = true;
-            }
-        }
     }
+    
 
-    // Basic wrapper for completion items
-    public class MyCompletionData : ICSharpCode.AvalonEdit.CodeCompletion.ICompletionData
-    {
-        public MyCompletionData(string text) => Text = text;
-        public System.Windows.Media.ImageSource Image => null;
-        public string Text { get; }
-        public object Content => Text;
-        public object Description => "Python keyword: " + Text;
-        public double Priority => 0;
-
-        public void Complete(TextArea textArea, ISegment completionSegment, EventArgs insertionRequestEventArgs)
-        {
-            textArea.Document.Replace(completionSegment, Text);
-        }
-    }
 }
